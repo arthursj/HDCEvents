c5be78e6379051b5603bcd693c61553b1797b43e
Arthur Silva <arthurcom898estudos@gmail.com>
2026-06-28 15:47:19 -0300

    refactor: create shared Blade layout and extend views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @extends('layouts.main')
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        @section('title', 'HDC Events')
 
-        <link type="text/css" rel="stylesheet" href="../css/styles.css">
-        <script src="/js/scripts.js"></script>
-    </head>
-    <body>
+        @section('content')
+
         <h1>Algum Título</h1>
         <img src="../img/banner.jpg" alt="Banner">
         @if(10 > 5)
@@ -43,6 +37,8 @@
             <p>{{ $nome }}</p>
         @endforeach
 
+        @endphp
+
         {{-- Esse é um comentário no Blade --}}
-    </body>
-</html>
+
+        @endsection
